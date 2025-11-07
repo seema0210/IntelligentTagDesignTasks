316553cd6537938a0e43f953efda0e042c90ef1a
Seema Nale <seemadnale02@gmail.com>
2025-11-07 20:10:27 +0530

    Added new functionality with Sub Total, Tip Amount

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import { AmountTip } from './components/tip_page/AmountTip'
+
+function App() {
+
+  return (
+    <>
+      <AmountTip/>
+    </>
+  )
+}
+
+export default App
---
+++ my-react-app/src/components/tip_page/AmountTip.jsx
@@ -0,0 +1,118 @@
+import React, { useState } from 'react'
+import './amountTip.css'
+import Calculator from './Calculator'
+
+
+export const AmountTip = () => {
+
+    const [openCalculator, setOpenCalculator] = useState(false)
+    const [changeAmount, setChangeAmount] = useState(100)
+    const [tipAmount, settipAmount] = useState(0.00)
+
+    const handleChangeAmount = (e) => {
+        let value = e.target.value.replace(/[^0-9.]/g, '')  // here accepting only numbers 
+        setChangeAmount(Number(value) || 0)
+    }
+
+    const amountWithDoller = (value) => {
+        let newNumber = +value
+        return `$${newNumber.toFixed(2)}`  // set $ sign with decimal points
+    }
+    let fiftinPercent = changeAmount * 0.15;
+    let twentyPercent = changeAmount * 0.20
+    let twentyFivePercent = changeAmount * 0.25
+
+    const handlePercentage = (val) => {
+        switch(val){
+            case 15 : settipAmount(fiftinPercent)
+            break;
+            case 20 : settipAmount(twentyPercent)
+            break;
+            case 25 : settipAmount(twentyFivePercent)
+            break
+        }
+        
+    }
+    
+  return (
+    <div class="home">
+         <nav>
+            <i className="fa-solid fa-angle-left"></i>
+        </nav>
+
+        <div className="home-body">
+            <div className="home-nav">
+                <div className="navbar">
+                    <h1>Check Subtotal :</h1>
+                    <input type="text"
+                    value={amountWithDoller(changeAmount)}
+                    onChange={handleChangeAmount}
+                    />
+                </div>
+                <p>Tip is based on the full check before comps or discounts</p>
+            </div>
+            <div className="home-amount">
+                <h1>Choose the ammount to tip</h1>
+                <div className="text-amount">
+                    <h1 >{amountWithDoller(tipAmount)}</h1>
+                </div>
+                <div className="percentage-data">
+                    <div className="percentage">
+                        <h4 onClick={()=>handlePercentage(15)}>15%</h4>
+                        <p>{amountWithDoller(fiftinPercent)}</p>
+                    </div>
+                    <div className="percentage">
+                        <h4 onClick={()=>handlePercentage(20)}>20%</h4>
+                        <p>{amountWithDoller(twentyPercent)}</p>
+                    </div>
+                    <div className="percentage">
+                        <h4 onClick={()=>handlePercentage(25)}>25%</h4>
+                        <p>{amountWithDoller(twentyFivePercent) }</p>
+                    </div>
+                    <div className="percentage">
+                        <h4 className="other" id="openPopup" onClick={()=> setOpenCalculator(true)}>
+                            OTHER
+                        </h4>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div className="show-amount">
+            <h1>TIP {amountWithDoller(tipAmount)}</h1>
+        </div>
+
+        <footer>
+            <div className="recet">
+                <i className="fa-solid fa-calculator"></i>
+                <p>[RECET]</p>
+            </div>
+
+            <div className="comps-apply">
+                <p>(Full Comps applied)</p>
+                <i className="fa-solid fa-file-invoice"></i>
+            </div>
+        </footer>
+
+    {
+        openCalculator && 
+        <div id="popupOverlay">
+            <i className="fa-solid fa-xmark cancel-icon" 
+            id="closePopupBtn" 
+            onClick={()=>setOpenCalculator(false)}>
+            </i>
+            <div className="calc-frame">
+            <Calculator 
+            tipAmount={tipAmount} 
+            settipAmount={settipAmount} 
+            setOpenCalculator={setOpenCalculator}
+            amountWithDoller={amountWithDoller}
+            />
+            </div>
+        </div>
+    }
+        
+
+    </div>
+  )
+}
