8d183b86338e83bfe6e15a25ef8fd58a944c1ca4
seema0210 <seemadnale02@gmail.com>
2025-11-19 19:32:51 +0530

    set amounts in input with validation and smart allocation and reset functionality

--- my-react-app/src/components/comps_page/Comps.jsx
+++ my-react-app/src/components/comps_page/Comps.jsx
@@ -5,7 +5,12 @@ import InputPopup from './InputPopup'
 
 const Comps = () => {
 
-    const [inputValue, setInputValue] = useState(9.50)
+    const [eCompTotal, setECompTotal] = useState(59.5)
+    const [easyDineVal, setEasyDineVal] = useState(9.50)
+    const [clubDollarVal, setCLubDillarVal] = useState(0.00)
+    const [eComVal, setEcomVal] = useState(0)
+    const [smartAllocate, setSmartAllocate] = useState(false)
+
     const [inputValPopup, setInputValPopup] = useState(false)
     const [popupTitle, setPopupTitle] = useState('')
 
@@ -17,6 +22,41 @@ const Comps = () => {
         setInputValPopup(true)
         setPopupTitle("Club Dollars")
     }
+    const handleEComps = (item) => {
+        if(eComVal == item){ // toggle functionality (if got repest value then it will reset the val)
+            setEcomVal(0)
+        }else{
+            setEcomVal(item)
+        }
+    }
+
+    var addChargedToComps = (+eComVal) + (+clubDollarVal) + (+easyDineVal) // all entered values are added
+    var chargedToComps = `$${addChargedToComps.toFixed(2)}` // fixed those values with 2 digits with $
+
+    var remainingAfterComps = eCompTotal - addChargedToComps
+    var remainingAfterCompsVal = `$${remainingAfterComps.toFixed(2)}`
+
+    var remainingAfterCompsValFinal = (remainingAfterComps > 0 && smartAllocate) ? `$${Number(0).toFixed(2)}` :
+     remainingAfterComps > 0 ? remainingAfterCompsVal : `$${Number(0).toFixed(2)}`
+
+
+    const handleSmartAllocate = () => {
+        if (remainingAfterComps > 0) {
+            setSmartAllocate((preVal) => !preVal)
+        }
+    }
+    const handleReset = () => {
+        setEasyDineVal(0)
+        setCLubDillarVal(0)
+        setEcomVal(0)
+        setSmartAllocate(false)
+    }
+    // console.log('eCompTotal', eCompTotal);
+    // console.log('addChargedToComps', addChargedToComps);
+    console.log('remainingAfterComps', remainingAfterComps);
+    // console.log('eComVal', eComVal);
+    // console.log('selectedComp', selectedComp);
+
 
     return (
         <div className="home">
@@ -28,100 +68,84 @@ const Comps = () => {
             <div className="navbar">
                 <h1>Comp-Eligible Total :</h1>
                 <input type="text"
-                    value="$59.50"
+                    value={`$${Number(eCompTotal).toFixed(2)}`}
                 />
             </div>
 
             <div className='comps-body'>
                 <h2 className='your-comps'>Apply Your Comps:</h2>
-                <p className='adjust'>Adjusted</p>
+                {
+                    (smartAllocate == true && remainingAfterComps > 0) ? <p className='adjust'>Adjusted</p>
+                        : (easyDineVal > 0 && remainingAfterComps > 0) ? <p className='adjust'> Applied</p> : <p style={{ height: "1vh" }}></p>
+                }
                 <div className='comps-easy-dine'>
                     <div className='comps-easy'>
                         <h2>EasyDine</h2>
-                        <p>Available Balance: $24.00</p>
+                        {/* <p style={{ color: 'red' }}>Available Balance: {`$${Number(easyDineVal).toFixed(2)}`}</p> */}
+                        <p style={{ color: remainingAfterComps < 0 ? 'red' : 'black' }}>Available Balance: {`$${Number(easyDineVal).toFixed(2)}`}</p>
+                        {/* <p>Available Balance: {`$${Number(easyDineVal).toFixed(2)}`}</p> */}
                     </div>
                     <input type="text"
-                        value="$9.50"
+                        value={`$${Number(easyDineVal).toFixed(2)}`}  // it will show 2 decimal place.
                         onClick={handleEasyDine}
                     />
                 </div>
 
                 <div className='e-comp'>
-                    <div className='e-comp-first'>
-                        <div className='e-comp-val'>
-                            <h2>eComp</h2>
-                            <p>$50.00</p>
-                        </div>
-                        <div className='e-comp-title'>
-                            <p className='date'>Expiration Date: 02/20/24</p>
-                            <p className='btn'>Applied</p>
-                        </div>
-                    </div>
-                    <div className='e-comp-first'>
-                        <div className='e-comp-val'>
-                            <h2>eComp</h2>
-                            <p>$25.00</p>
-                        </div>
-                        <div className='e-comp-title'>
-                            <p className='date'>Expiration Date: 02/20/24</p>
-                            <p className='btn'>Applied</p>
-                        </div>
-                    </div>
-                    <div className='e-comp-first'>
-                        <div className='e-comp-val'>
-                            <h2>eComp</h2>
-                            <p>$10.00</p>
-                        </div>
-                        <div className='e-comp-title'>
-                            <p className='date'>Expiration Date: 02/20/24</p>
-                            <p className='btn'>Applied</p>
-                        </div>
-                    </div>
-                    <div className='e-comp-first'>
-                        <div className='e-comp-val'>
-                            <h2>eComp</h2>
-                            <p>$5.00</p>
-                        </div>
-                        <div className='e-comp-title'>
-                            <p className='date'>Expiration Date: 02/20/24</p>
-                            <p className='btn'>Applied</p>
-                        </div>
-                    </div>
+                    {
+                        [50, 25, 10, 5]?.map((item) => (
+                            <div className='e-comp-first'>
+                                <div className='e-comp-val'>
+                                    <h2>eComp</h2>
+                                    <p>${item}</p>
+                                </div>
+                                <div className='e-comp-title'>
+                                    <p className='date'>Expiration Date: 02/20/24</p>
+                                    <button className={eComVal == item ? 'btn' : 'btn-apply' }
+                                        onClick={()=>handleEComps(item)}
+                                    >{ eComVal == item ? "Applied" : "Tap to apply"}</button>
+                                </div>
+                            </div>
+                        ))
+                    }
 
-                    {/* <div className='e-comp-second'>
-                        <h2>eComp</h2>
-                        <p>Expiration Date :  02/20/24</p>
-                    </div> */}
 
                 </div>
 
-
+                {
+                    (smartAllocate == true && remainingAfterComps > 0) ? <p className='adjust'>Adjusted</p>
+                        : (clubDollarVal > 0 && remainingAfterComps > 0) ? <p className='adjust'> Applied</p> : <p></p>
+                }
                 <div className='club-dollars'>
                     <div className='dollars'>
                         <h2>Club Dollars</h2>
-                        <p>Available Balance: $65.00</p>
+                        {/* <p>Available Balance: {`$${Number(clubDollarVal).toFixed(2)}`}</p> */}
+                        <p style={{ color: remainingAfterComps < 0 ? 'red' : 'black' }}>Available Balance: {`$${Number(clubDollarVal).toFixed(2)}`}</p>
                     </div>
                     <input type="text"
-                        value="$0.00"
+                        value={`$${Number(clubDollarVal).toFixed(2)}`}
                         onClick={handleClubDollars}
                     />
                 </div>
 
                 <div className='buttons'>
-                    <button className='btn1'><i class="fas fa-lightbulb"></i> Smart Allocate</button>
-                    <button className='btn2'> <i className="fas fa-redo"></i>Reset</button>
+                    <button className={smartAllocate ? "btn1" :'btn-new1'} onClick={handleSmartAllocate}>
+                        <i class="fas fa-lightbulb"></i> Smart Allocate</button>
+                    <button className='btn2' onClick={handleReset}>
+                        <i className="fas fa-redo" ></i>Reset</button>
                 </div>
 
             </div>
 
             <div className='total-comps'>
                 <div className='charged-t0-comps'>
-                    <p className='title'>Changed to Comps:</p>
-                    <p>$59.50</p>
+                    <p className='title'>Charged to Comps:</p>
+                    <p style={{color : remainingAfterComps < 0 ? 'red' : 'black', 
+                    fontWeight: remainingAfterComps < 0 ? 'bold' : 500}} >{chargedToComps}</p>
                 </div>
                 <div className='after-comps'>
                     <p className='title'>Remaining After Comps:</p>
-                    <p>$0.00</p>
+                    <p>{remainingAfterCompsValFinal}</p>
                 </div>
             </div>
 
@@ -151,10 +175,11 @@ const Comps = () => {
                     </i>
                     <div className="calc-frame">
                         <InputPopup
-                        setInputValPopup={setInputValPopup}
-                        popupTitle={popupTitle}
-                        setInputValue={setInputValue}
-                        inputValue={inputValue}
+                            setInputValPopup={setInputValPopup}
+                            popupTitle={popupTitle}
+                            setInputValue={popupTitle === "EasyDine" ? setEasyDineVal : setCLubDillarVal}
+                            inputValue={popupTitle === "EasyDine" ? easyDineVal : clubDollarVal}
+                            eCompTotal={eCompTotal}
                         />
                     </div>
                 </div>
